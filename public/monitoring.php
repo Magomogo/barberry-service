7941e88e45f52fbc2fbb5f34edc4b36544e210d4
Magomogo <mgnatenko@gmail.com>
2012-11-12 15:57:48 +0700

    + monitoring checks own writable directories

--- public/monitoring.php
+++ public/monitoring.php
@@ -4,4 +4,12 @@ $RunningMonitorFunction = 'reportMalfunction';
 
 $errors = include dirname(__DIR__) . '/inc/RunMonitorFunction.php';
 
+if (!is_writeable(__DIR__ . '/cache')) {
+    $errors[] = 'Cannot write to ./public/cache directory';
+}
+
+if (!is_writeable(dirname(__DIR__) . '/usr/storage')) {
+    $errors[] = 'Cannot write to ./usr/storage directory';
+}
+
 echo count($errors) > 0 ? implode("<br>", $errors) : "ok";
